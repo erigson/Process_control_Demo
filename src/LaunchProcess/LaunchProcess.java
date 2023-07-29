7b66473a21e4388ca0dd9789a0dd826332a4740d
Eric <ericissacgalanter@gmail.com>
2023-07-29 16:02:06 -0400

    Implemented Command Line Args

--- src/LaunchProcess/LaunchProcess.java
+++ src/LaunchProcess/LaunchProcess.java
@@ -4,21 +4,32 @@ import java.io.*;
 
 public class LaunchProcess {
 
-    public static void main(String[] args) throws IOException, Exception {
+    public static void main(String[] args) {
 
-        if(args.length < 2) {
-            System.out.println("Please provide the class name and file path as arguments.");
+        // Check if command line argument is provided
+        if(args.length < 1) {
+            System.out.println("No file path provided. Please provide a file path as command line argument.");
+            System.exit(1);
+        }
+
+        String filePath = args[0];
+
+        // Command to open the file
+        String command;
+        String os = System.getProperty("os.name").toLowerCase();
+
+        // Check for operating system
+        if (os.contains("win")) {
+            command = "cmd /c start " + filePath;
+        } else if (os.contains("nix") || os.contains("nux") || os.contains("mac")) {
+            command = "open " + filePath;
+        } else {
+            System.out.println("Unsupported operating system. This program supports Windows, Linux and MacOS only.");
+            System.exit(1);
             return;
         }
 
-        String className = args[0]; // The first argument is the Java class to be executed.
-        String filePath = args[1]; // The second argument is the file path.
-
-        String javaPath = "java";
-        String command = javaPath + " " + className + " " + filePath;
-
         try {
-
             long startTime = System.currentTimeMillis();
 
             Process process = Runtime.getRuntime().exec(command);
@@ -29,18 +40,14 @@ public class LaunchProcess {
             while ((line = is.readLine()) != null)
                 System.out.println(line);
 
-
             int exit = process.waitFor();
             long endTime = System.currentTimeMillis();
 
-            // checking the exit value of subprocess
             System.out.println("exit value:" + process.exitValue());
             System.out.println("Time to finish all tasks: " + (endTime - startTime) + " ms");
         }
 
-        // Catch block to handle the exceptions
         catch (Exception ex) {
-
             ex.printStackTrace();
         }
     }
